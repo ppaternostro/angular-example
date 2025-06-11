c6f224bda5157ce0b8522c6dafa563e173aec9a2
Pat Paternostro <ppaternostro@yahoo.com>
2025-06-11 13:23:34 -0400

    Post service test updates

--- src/app/service/rest/post/post.service.spec.ts
+++ src/app/service/rest/post/post.service.spec.ts
@@ -1,19 +1,115 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+  TestRequest,
+} from '@angular/common/http/testing';
+
+import { HttpHandler, provideHttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { mockPost } from '../../../../mock/model/post/mock-post';
+import { Post } from '../../../model/post';
 import { PostService } from './post.service';
 
 describe('PostService', () => {
   let service: PostService;
+  let backEnd: HttpTestingController;
+  let baseUrl: string;
+  let tracker: Subscription;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpClient, HttpHandler],
+      providers: [HttpHandler, provideHttpClient(), provideHttpClientTesting()],
     });
     service = TestBed.inject(PostService);
+    backEnd = TestBed.inject(HttpTestingController);
+    baseUrl = 'https://jsonplaceholder.typicode.com/posts';
+    tracker = new Subscription();
+  });
+
+  afterEach(() => {
+    backEnd.verify();
+
+    tracker.unsubscribe();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve a single post when retrieving by id', () => {
+    tracker.add(
+      service.getPost(mockPost.id).subscribe((result: Post) => {
+        expect(result).toEqual(mockPost);
+      })
+    );
+
+    const call: TestRequest = backEnd.expectOne({
+      url: `${baseUrl}/${mockPost.id}`,
+      method: 'GET',
+    });
+
+    call.flush(mockPost);
+  });
+
+  it('should retrieve multiple posts', () => {
+    tracker.add(
+      service.getPosts().subscribe((result: Post[]) => {
+        expect(result).toEqual([mockPost]);
+      })
+    );
+
+    const call: TestRequest = backEnd.expectOne({
+      url: `${baseUrl}`,
+      method: 'GET',
+    });
+
+    call.flush([mockPost]);
+  });
+
+  it('should update the specified post', () => {
+    tracker.add(
+      service.savePost(mockPost).subscribe((result: Post) => {
+        expect(result).toEqual(mockPost);
+      })
+    );
+
+    const call: TestRequest = backEnd.expectOne({
+      url: `${baseUrl}/${mockPost.id}`,
+      method: 'PUT',
+    });
+
+    call.flush(mockPost);
+  });
+
+  it('should delete the specified post', () => {
+    tracker.add(
+      service.deletePost(mockPost).subscribe((result: Post) => {
+        expect(result).toEqual(mockPost);
+      })
+    );
+
+    const call: TestRequest = backEnd.expectOne({
+      url: `${baseUrl}/${mockPost.id}`,
+      method: 'DELETE',
+    });
+
+    call.flush(mockPost);
+  });
+
+  it('should create the specified post', () => {
+    tracker.add(
+      service.createPost(mockPost).subscribe((result: Post) => {
+        expect(result).toEqual(mockPost);
+      })
+    );
+
+    const call: TestRequest = backEnd.expectOne({
+      url: `${baseUrl}`,
+      method: 'POST',
+    });
+
+    call.flush(mockPost);
   });
 });
---
+++ src/mock/model/post/mock-post.ts
@@ -0,0 +1,8 @@
+import { Post } from '../../../app/model/post';
+
+export const mockPost: Post = {
+  id: 1,
+  userId: 1,
+  title: 'Title',
+  body: 'Body',
+};
