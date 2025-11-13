830db4a766005f93482c2c3ac2f393e03964480a
Pat Paternostro <ppaternostro@yahoo.com>
2025-11-12 23:07:16 -0500

    Fix broken tests

--- src/app/component/search/search.component.spec.ts
+++ src/app/component/search/search.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { HttpHandler, provideHttpClient } from '@angular/common/http';
+import { MatSelectHarness } from '@angular/material/select/testing';
 import { By } from '@angular/platform-browser';
 import { PostService } from '../../service/rest/post/post.service';
 import { SearchComponent } from './search.component';
@@ -8,6 +11,7 @@ import { SearchComponent } from './search.component';
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,16 +22,14 @@ describe('SearchComponent', () => {
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should ensure default state on creation', () => {
-    const searchBy: HTMLElement = fixture.debugElement.query(
-      By.css('#search-by')
-    ).nativeElement;
+  it('should ensure default state on creation', async () => {
     const searchText = fixture.debugElement.query(
       By.css('#search-text')
     ).nativeElement;
@@ -35,21 +37,21 @@ describe('SearchComponent', () => {
       By.css('#search')
     ).nativeElement;
 
-    expect(searchBy.getAttribute('ng-reflect-value')).toBe('All');
+    const selectHarness = await loader.getHarness(MatSelectHarness);
+    const selectedValue = await selectHarness.getValueText();
+
+    expect(selectedValue).toBe('All');
     expect(searchText.disabled).toBeTrue();
     expect(searchButton.disabled).toBeFalse();
   });
 
-  it('should ensure correct state on selection change', () => {
+  it('should ensure correct state on selection change', async () => {
     // Change selection value
     component.selected = 'Id';
     component.onSelectionChanged(component.selected);
 
     fixture.detectChanges();
 
-    const searchBy: HTMLElement = fixture.debugElement.query(
-      By.css('#search-by')
-    ).nativeElement;
     const searchText = fixture.debugElement.query(
       By.css('#search-text')
     ).nativeElement;
@@ -57,7 +59,10 @@ describe('SearchComponent', () => {
       By.css('#search')
     ).nativeElement;
 
-    expect(searchBy.getAttribute('ng-reflect-value')).toBe('Id');
+    const selectHarness = await loader.getHarness(MatSelectHarness);
+    const selectedValue = await selectHarness.getValueText();
+
+    expect(selectedValue).toBe('Id');
     expect(searchText.disabled).toBeFalse();
     expect(searchButton.disabled).toBeTrue();
 
